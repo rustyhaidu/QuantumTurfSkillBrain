85e40152c4ed034d53ea9b3a95d6d26906017f36
Claudiu Haidu <claudiu.haidu@globant.com>
2025-02-27 21:24:45 +0200

    registrationInvalidEmail complete

--- src/test/java/org/quantumturf/Registration.java
+++ src/test/java/org/quantumturf/Registration.java
@@ -1,11 +1,9 @@
 package org.quantumturf;
 
-
-import org.junit.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-//import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -31,21 +29,30 @@ public class Registration {
         driver.findElement(By.xpath("//span[.='Privacy Policy']/preceding-sibling::span")).click();
         driver.findElement(By.xpath("//span[.='Tems and Conditions']/preceding-sibling::span")).click();
         WebElement getStarted = driver.findElement(By.cssSelector("button.btn"));
-        Actions actions=  new Actions(driver);
+        Actions actions = new Actions(driver);
         actions.moveToElement(getStarted).click().perform();
-        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.text-red")));
 
         Assert.assertTrue(alert.isDisplayed());
-        Assert.assertEquals( alert.getText(),"This username already exists");
-
-
-
-
-
-
-
+        Assert.assertEquals(alert.getText(), "This username already exists");
     }
 
+    @Test
+    public void registrationInvalidEmail() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.quantumturf.com");
+        driver.findElement(By.cssSelector(".header a[href*='signup'")).click();
+        driver.findElement(By.xpath("//div[.='Email']/following-sibling::div/input"))
+                .sendKeys("alinamarie0426gmail.com");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//div[.='Email']/following-sibling::div[@class='text-red text-small mt-1']")));
+
+        Assert.assertTrue(errorMessage.isDisplayed());
+        Assert.assertEquals(errorMessage.getText(), "Invalid email format.");
+    }
 }
