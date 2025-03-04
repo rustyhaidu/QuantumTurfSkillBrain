4cf9f654eab3c92ad9ded2f67534f2bf049258eb
Claudiu Haidu <claudiu.haidu@globant.com>
2025-03-04 19:44:31 +0200

    update tests

---
+++ src/main/java/org/quantumturf/Constants.java
@@ -0,0 +1,5 @@
+package org.quantumturf;
+
+public class Constants {
+    public static final String FIRST_PARAGRAPH_FAQ = "Quantum Turf stands out by using cutting-edge AI and turfgrass algorithms to provide solutions to key issues faced by lawn care companies, making it an advanced agronomic tool. While we are not currently a fully operational CRM, we are the agronomic tool that builds programs that convert lawns into masterpieces at a budget that keeps you profitable. ";
+}
--- src/test/java/org/quantumturf/CitireParagraf.java
+++ src/test/java/org/quantumturf/CitireParagraf.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -18,16 +20,30 @@ public class CitireParagraf {
         driver.manage().window().maximize();
         driver.get("https://quantumturf.com/?trk=public_post_main-feed-card-text");
 
-        WebDriverWait wait = new WebDriverWait(driver ,Duration.ofSeconds(10) );
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        WebElement makesButton = driver.findElement(By.cssSelector("#hs_cos_wrapper_widget_1739158895626 > " +
-                "section > details:nth-child(1) > summary"));
+        /*WebElement makesButton = driver.findElement(By.cssSelector("#hs_cos_wrapper_widget_1739158895626 > " +
+                "section > details:nth-child(1) > summary"));*/
+        // WebElement makesButton = driver.findElement(By.cssSelector("details:nth-child(1)"));
+
+        //WebElement makesButton = driver.findElement(By.xpath("//*[.='What makes Quantum Turf different from other lawn care software?']"));
+        WebElement makesButton = driver.findElement(By.xpath("//*[.='What makes Quantum Turf different from other lawn care software?']"));
+
         makesButton.click();
 
-        WebElement citireText = driver.findElement(By.cssSelector("#hs_cos_wrapper_widget_1739158895626 >" +
-                " section > details:nth-child(1) > div > div > p > span"));
+/*        WebElement citireText = driver.findElement(By.cssSelector("#hs_cos_wrapper_widget_1739158895626 >" +
+                " section > details:nth-child(1) > div > div > p > span"));  */
+        //WebElement citireText = driver.findElement(By.cssSelector("details[open] span"));
+        // WebElement citireText = driver.findElement(By.xpath("//details[@open]//span"));
 
-       String textExtras =  citireText.getText();
-        System.out.println("Textul este :" + textExtras );
+        WebElement citireText = driver.findElement(By.xpath("//details[@open]//span"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(citireText).click().perform();
+
+        String expectedText = Constants.FIRST_PARAGRAPH_FAQ;
+        String textExtras = citireText.getText();
+        System.out.println("Textul este :" + textExtras);
+
+        Assert.assertEquals(textExtras, expectedText, "Verificam ca textele coincid");
     }
 }
