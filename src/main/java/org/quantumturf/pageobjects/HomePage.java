4d9bc10245991d8fe1cc3b0e1b1fdf0f27ba9827
Claudiu Haidu <claudiu.haidu@globant.com>
2025-03-04 21:04:26 +0200

    page object model

--- src/main/java/org/quantumturf/pageobjects/HomePage.java
+++ src/main/java/org/quantumturf/pageobjects/HomePage.java
@@ -1,4 +1,20 @@
 package org.quantumturf.pageobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    WebDriver driver;
+
+    private By registerButtonSelector = By.cssSelector(".header a[href*='signup']");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+       // PageFactory.initElements(driver, HomePage.class);
+    }
+
+    public void clickOnRegisterButton() {
+        driver.findElement(registerButtonSelector).click();
+    }
+
 }
