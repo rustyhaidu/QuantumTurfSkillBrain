0d44c849c67f43107f7429a89b3eb57cc2470d56
Eusebiu1323 <eusebiusamartean@gmail.com>
2025-03-25 19:58:31 +0100

    PropertyTest

---
+++ src/test/java/org/quantumturf/propertytest/PropertyTest.java
@@ -0,0 +1,31 @@
+package org.QuantumTurf.propertytest;
+
+import org.quantumturf.BaseTest;
+import org.quantumturf.pageobjects.authorization.LoginPage;
+import org.quantumturf.pageobjects.mainpage.MainPage;
+import org.quantumturf.pageobjects.propertypage.PropertyPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PropertyTest extends BaseTest {
+    LoginPage loginPage;
+    MainPage mainPage;
+    PropertyPage propertyPage;
+
+    @BeforeMethod
+    public void setupPropertyPages(){
+        loginPage=new LoginPage(driver,wait);
+        mainPage=new MainPage(driver,wait);
+        propertyPage=new PropertyPage(driver,wait);
+    }
+
+    @Test
+    public void filterPropertyByAddress(){
+        loginPage.performLogin();
+        mainPage.clickOnPropertyTab();
+        propertyPage.searchProperty("Adresa12");
+        Assert.assertTrue(propertyPage.isPropertyFound("Adresa12"));
+    }
+}
+
