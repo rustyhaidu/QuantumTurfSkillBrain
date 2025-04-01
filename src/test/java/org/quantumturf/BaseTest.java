8d8490c5a454fed797a1842ad06ad606d310451b
Claudiu Haidu <claudiu.haidu@globant.com>
2025-04-01 21:40:36 +0300

    maps test

--- src/test/java/org/quantumturf/BaseTest.java
+++ src/test/java/org/quantumturf/BaseTest.java
@@ -1,6 +1,5 @@
 package org.quantumturf;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,13 +9,13 @@ import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
 
 public class BaseTest {
-    public WebDriver driver;
+    public ChromeDriver driver;
     public WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--force-device-scale-factor=0.75");
+        // options.addArguments("--force-device-scale-factor=0.75");
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
